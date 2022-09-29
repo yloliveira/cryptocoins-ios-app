0adb074b7ed4c71ba97c7c8023d9bf36ab94d509
Yan Oliveira <oliveira.yan02@gmail.com>
2022-09-29 15:40:45 -0300

    feat: link outlets and add picker data

--- cryptocoins/Controller/HomeViewController.swift
+++ cryptocoins/Controller/HomeViewController.swift
@@ -8,7 +8,31 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var coinNameLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let currenciesArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPicker.dataSource = self
     }
 }
+
+//MARK: - UIPickerViewDataSource
+
+extension HomeViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currenciesArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currenciesArray[row]
+    }
+}
+
